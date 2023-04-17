c37cfd13ca8f66e48b46dc20cbcd864e0a4d27af
santhana krishnan <69023009+krishnan1234@users.noreply.github.com>
2023-04-17 20:15:15 +0530

    Add files via upload

---
+++ variable and datatypes.py
@@ -0,0 +1,23 @@
+# Think of a variable as a name attached to a particular object. 
+# In Python, variables need not be declared or defined in advance, as is the case in many other programming languages. 
+# To create a variable, you just assign it a value and then start using it. 
+# Assignment is done with a single equals sign (=):
+# a=20
+# print(a)
+
+# Python also allows chained assignment, which makes it possible to assign the same value to several variables simultaneously:
+# a=b=c = 3
+# print(a,b,c)
+
+# In many programming languages, variables are statically typed. That means a variable is initially declared to have a specific data type, and any value assigned to it during its lifetime must always have that type.
+
+# Variables in Python are not subject to this restriction. In Python, a variable may be assigned a value of one type and then later re-assigned a value of a different type:
+
+a=20 #int
+b=3.45 #float
+c="santkrish" #string
+d=True #boolean
+print(a)
+print(b)
+print(c)
+print(d)
